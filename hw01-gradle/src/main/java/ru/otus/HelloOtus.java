2f8daa668e97b82047b974f82a21e6eb2be17633
pastetexthere <DAlePotapov@sberbank.ru>
2022-09-27 00:19:28 +0300

    add java version

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class HelloOtus {
     public static void main(String[] args) {
-        List<Integer> numbers = Lists.newArrayList(1, 2, 3);
+        List<Integer> numbers = Lists.newArrayList(1, 2, 3, 4);
         System.out.println(numbers);
     }
 
