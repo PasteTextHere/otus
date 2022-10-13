9e586fce1b4de1db86edb5a2e22a29f1f11b4937
Dmitry Potapov <DAlePotapov@sberbank.ru>
2022-10-14 00:49:08 +0300

    HW02

---
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -0,0 +1,31 @@
+package homework;
+
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class CustomerService {
+
+    private final Map<Customer, String> customerMap = new TreeMap<>(Comparator.comparing(Customer::getScores));
+
+    public Entry<Customer, String> getSmallest() {
+        Entry<Customer, String> smallest = customerMap.entrySet().stream().findFirst().orElse(null);
+        try {
+            return new AbstractMap.SimpleEntry<>(new Customer(smallest.getKey()), smallest.getValue());
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    public Entry<Customer, String> getNext(Customer customer) {
+        Set<Entry<Customer, String>> entryList = customerMap.entrySet();
+        Optional<Entry<Customer, String>> tmp = entryList.stream().filter(entry -> entry.getKey().getScores() > customer.getScores()).findFirst();
+        if (tmp.isPresent()) {
+            return new AbstractMap.SimpleEntry<>(new Customer(tmp.get().getKey()), tmp.get().getValue());
+        } else return null;
+    }
+
+    public void add(Customer customer, String data) {
+        customerMap.put(customer, data);
+    }
+}
