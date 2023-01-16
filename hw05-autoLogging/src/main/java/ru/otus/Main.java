05f7fdf943a7a9f1261c53b3ac9358dec6058666
Dmitry Potapov <DAlePotapov@sberbank.ru>
2023-01-16 23:12:26 +0300

    format main method with option-comm-l

--- hw05-autoLogging/src/main/java/ru/otus/Main.java
+++ hw05-autoLogging/src/main/java/ru/otus/Main.java
@@ -6,11 +6,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         var testLogging = ProxyForLogging.createProxyClass(TestLoggingImpl.class);
 
-        testLogging.calculation(1);
-        testLogging.calculation(1,4, "test");
-        testLogging.calculation(1,4);
         testLogging.calculation();
-        testLogging.calculation(1,4);
+        testLogging.calculation(1);
+        testLogging.calculation(1, 4);
+        testLogging.calculation(1, 4, "test");
 
     }
 }
